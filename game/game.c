ef14deb0a0179c1499da98353bdddc29114669ea
Emmy <32604310+emm218@users.noreply.github.com>
2024-06-30 11:49:08 -0400

    fix name casing :3

--- game/game.c
+++ game/game.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Emmy Emmycelium
+ * Copyright (C) 2024 emmy emmycelium
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
